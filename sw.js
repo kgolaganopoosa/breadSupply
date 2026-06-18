5d9cafcdb297de2b0f2e618db3ba9be554e0825a
kgolaganopoosa <kgolaganopeterpoo@gmail.com>
2026-06-18 04:09:18 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,9 @@
-const CACHE_NAME = 'bread-supply-v1';
-const ASSETS = [
-  '/',
-  '/index.html'
-];
-
+// Minimal service worker (sw.js)
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+  // Pass through all requests
+  event.respondWith(fetch(event.request));
 });
